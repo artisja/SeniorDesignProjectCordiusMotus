f5494c8a1b74eec23f7190048ea1cfc1ae109f58
artisja <artisja@vcu.edu>
2016-09-25 14:19:15 -0400

    Initial commit

---
+++ app/src/main/java/mult_603/seniordesignprojectcordiusmotus/UserDefinitionActivity.java
@@ -0,0 +1,13 @@
+package mult_603.seniordesignprojectcordiusmotus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserDefinitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_definition);
+    }
+}
