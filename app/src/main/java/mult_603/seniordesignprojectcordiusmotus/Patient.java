d73ff588eb10c8c727a83995f5f41ed52eb828fc
Wes <bosmanwn@vcu.edu>
2016-10-01 19:24:48 -0400

    Made a person class to interact with database

--- app/src/main/java/mult_603/seniordesignprojectcordiusmotus/Patient.java
+++ app/src/main/java/mult_603/seniordesignprojectcordiusmotus/Patient.java
@@ -1,5 +1,13 @@
 package mult_603.seniordesignprojectcordiusmotus;
 
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 /**
  * Created by Wes on 10/1/16.
  */
@@ -8,6 +16,8 @@ public class Patient {
     public String patientUserName;
     private double heartRate;
     private double temperature;
+    private String uuid;
+    public static final String TAG = Patient.class.getSimpleName();
 
     public Patient(){
 
@@ -25,6 +35,31 @@ public class Patient {
         return patientUserName;
     }
 
+    public String getUuid(){
+        return uuid;
+    }
+
+    // Add a patient object to the Firebase Database using a string reference
+    public void addPatientToDatabase(Patient patient, String reference){
+        FirebaseDatabase fireDb = FirebaseDatabase.getInstance();
+        DatabaseReference ref = fireDb.getReference(reference);
+        ref.setValue(patient);
+
+        // What if we could call this whenever the users position changes?
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String data = dataSnapshot.getValue(String.class);
+                Log.i(TAG, "Persons data has changed: " + data);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.i(TAG, "Persons data change was cancelled");
+            }
+        });
+    }
+
     public void setHeartRate(double hr){
         this.heartRate = hr;
     }
@@ -37,6 +72,9 @@ public class Patient {
         this.patientUserName = uName;
     }
 
+    public void setUuid(String id){
+        this.uuid = id;
+    }
     @Override
     public String toString(){
         String str = patientUserName + " " + heartRate + " " + temperature + " ";
