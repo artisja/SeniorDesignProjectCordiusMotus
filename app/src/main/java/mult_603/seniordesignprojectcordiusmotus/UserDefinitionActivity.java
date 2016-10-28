0a9c1c35ceeedfddafc546653711c04a5be416dc
artisja <artisja@vcu.edu>
2016-10-28 19:19:12 -0400

    add new way to get location data using location manager

--- app/src/main/java/mult_603/seniordesignprojectcordiusmotus/UserDefinitionActivity.java
+++ app/src/main/java/mult_603/seniordesignprojectcordiusmotus/UserDefinitionActivity.java
@@ -1,6 +1,11 @@
 package mult_603.seniordesignprojectcordiusmotus;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,13 +13,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import static android.Manifest.permission.SEND_SMS;
-
 public class UserDefinitionActivity extends AppCompatActivity {
 
-    private Button medicButton,caliButton, bluetoothButton;
+    private Button medicButton, caliButton, bluetoothButton;
     private Intent medicMapIntent;
     private Intent bluetoothActivity;
+    public double longitude, latitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,6 @@ public class UserDefinitionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_definition);
         findViews();
         setButtonDestination();
-
     }
 
     private void setButtonDestination() {
@@ -30,7 +33,7 @@ public class UserDefinitionActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(UserDefinitionActivity.this, "Emergency Contact/Edit screen", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
             }
         });
@@ -40,6 +43,8 @@ public class UserDefinitionActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Toast.makeText(UserDefinitionActivity.this, "Google Maps/medical data view screen", Toast.LENGTH_SHORT).show();
                 medicMapIntent = new Intent(getApplicationContext(), LoginDeviceLocator.class);
+                medicMapIntent.putExtra("Longitude",longitude);
+                medicMapIntent.putExtra("Latitude", latitude);
                 startActivity(medicMapIntent);
             }
         });
@@ -59,4 +64,42 @@ public class UserDefinitionActivity extends AppCompatActivity {
         bluetoothButton = (Button) findViewById(R.id.bluetooth_Button);
     }
 
+    public void getLocationManager() {
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(Location location) {
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+    }
 }
